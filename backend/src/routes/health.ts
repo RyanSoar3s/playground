3254f5596ad89de36ddc4655a63cf14674551713
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-03 10:34:19 -0300

    feat: add health route

---
+++ backend/src/routes/health.ts
@@ -0,0 +1,23 @@
+import { Origin } from "../../config/env";
+import type { HealthResult } from "../models/response.model";
+
+const Health = () => {
+  const reponse = JSON.stringify({
+    status: "ok",
+    error: null
+
+  } satisfies HealthResult);
+
+  return new Response(reponse, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": Origin || "http://localhost:4200",
+      "Content-Type": "application/json"
+
+    }
+
+  })
+
+};
+
+export default Health
