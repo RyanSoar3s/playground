cb8540ae110d024e6a8f8a6eeaab7db424ac0bd0
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-08 18:59:36 -0300

    feat: add monaco editor component and add health route to check the server

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CodeEditor } from './features/components/code-editor/code-editor';
+import { Api } from './core/services/api';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    CodeEditor
+
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  private api = inject(Api);
+
+  ngOnInit(): void {
+    this.api.checkHealth().subscribe({
+      next: (data) => {
+        console.log(`[Server] {\n  status: '${data.status}',\n  error: ${data.error}\n}`);
+
+      },
+      error: (err) => {
+        const status = ("status" in err && typeof err.status === "string") ? (err.status) : "error";
+        const error = (typeof err === "object") ? JSON.stringify(err) : err;
+
+        console.error(`[Server] {\n  status: '${status}',\n  error: ${error}\n}`);
+
+      }
+
+    });
+
+  }
+
 }
