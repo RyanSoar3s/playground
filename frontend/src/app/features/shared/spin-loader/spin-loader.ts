1eab014d6d029004461999bc2b0617e2d57ee73c
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-09 18:19:55 -0300

    feat: update spin-loader styles to include dynamic border width

--- frontend/src/app/features/shared/spin-loader/spin-loader.ts
+++ frontend/src/app/features/shared/spin-loader/spin-loader.ts
@@ -7,12 +7,15 @@ import { Component, input } from '@angular/core';
   styleUrl: './spin-loader.scss',
   host: {
     '[style.height]': 'height()',
-    '[style.width]': 'width()'
+    '[style.width]': 'width()',
+    '[style.borderWidth]': 'borderWidth()'
 
   }
 })
 export class SpinLoader {
   height = input.required<string>();
   width = input.required<string>();
+
+  borderWidth = input<string>("3px");
 
 }
