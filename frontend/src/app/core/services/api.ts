899523921e13300585dae5bc53160d03c39a65cf
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-12 13:28:06 -0300

    feat(Api): implement request method for code execution route

--- frontend/src/app/core/services/api.ts
+++ frontend/src/app/core/services/api.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LanguageList } from '../../models/language-list.model';
 import { environment } from '../../../environments/environment';
+import { ExecutionCode, ExecutionResult } from '../../models/code-execution.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,13 +12,18 @@ export class Api {
   private http = inject(HttpClient);
   private readonly baseUrl = environment.apiUrl;
 
-  checkHealth(): Observable<{ status: "ok" | "error", error: null | string }> { 
+  checkHealth(): Observable<{ status: "ok" | "error", error: null | string }> {
     return this.http.get<{ status: "ok" | "error", error: null | string }>(`${this.baseUrl}/health`);
 
   }
 
   getLanguages(): Observable<LanguageList> {
     return this.http.get<LanguageList>(`${this.baseUrl}/languages`);
+
+  }
+
+  executeCode(code: ExecutionCode): Observable<ExecutionResult> {
+    return this.http.post<ExecutionResult>(`${this.baseUrl}/execute`, code, { credentials: "include" });
 
   }
 
