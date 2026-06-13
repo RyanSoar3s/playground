68c8f45919873ebd280b8bc5632c24d50797174e
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-13 18:17:25 -0300

    feat(routes): update routing to include error component and guards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,30 @@
 import { Routes } from '@angular/router';
-import { CodeEditor } from './features/components/code-editor/code-editor';
 import { getLanguagesResolver } from './core/resolvers/get-languages-resolver';
+import { Error } from './features/shared/error/error';
+import { healthGuard } from './core/guards/health-guard';
+import { CodeEditor } from './features/components/code-editor/code-editor';
+import { errorGuard } from './core/guards/error-guard';
 
 export const routes: Routes = [
   {
     path: "",
     component: CodeEditor,
+    canActivate: [ healthGuard ],
     resolve: {
       languageList: getLanguagesResolver
 
     }
+
+  },
+  {
+    path: "error",
+    component: Error,
+    canActivate: [ errorGuard ]
+
+  },
+  {
+    path: "**",
+    redirectTo: ""
 
   }
 
