fd1ce830982e441a22f9866e01040072476beb92
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-13 21:19:53 -0300

    feat(app): implement loading skeleton and health check integration

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GetLanguages } from '@core/services/get-languages';
+import { LoadingSkeleton } from '@features/shared/loading-skeleton/loading-skeleton';
+
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
-
+    RouterOutlet,
+    LoadingSkeleton
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {}
+export class App {
+  private languagesServices = inject(GetLanguages);
+
+  protected readonly languages = this.languagesServices.languages;
+  protected readonly errors = this.languagesServices.errors;
+
+  protected readonly isAppLoading = computed(() => this.languages() === null && this.errors() === null);
+
+}
