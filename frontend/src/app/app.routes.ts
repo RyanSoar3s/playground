6b2d501cf51ecc704a31f31ae8abac46a15267c8
RyanSoar3s <ryansoares.dev@gmail.com>
2026-06-13 21:20:09 -0300

    fix(routes): update import paths for consistency and clarity

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { getLanguagesResolver } from './core/resolvers/get-languages-resolver';
-import { Error } from './features/shared/error/error';
-import { healthGuard } from './core/guards/health-guard';
-import { CodeEditor } from './features/components/code-editor/code-editor';
-import { errorGuard } from './core/guards/error-guard';
+import { getLanguagesResolver } from '@core/resolvers/get-languages-resolver';
+import { Error } from '@features/shared/error/error';
+import { healthGuard } from '@core/guards/health-guard';
+import { CodeEditor } from '@features/components/code-editor/code-editor';
+import { errorGuard } from '@core/guards/error-guard';
 
 export const routes: Routes = [
   {
@@ -12,20 +12,15 @@ export const routes: Routes = [
     canActivate: [ healthGuard ],
     resolve: {
       languageList: getLanguagesResolver
-
     }
-
   },
   {
     path: "error",
     component: Error,
     canActivate: [ errorGuard ]
-
   },
   {
     path: "**",
     redirectTo: ""
-
   }
-
 ];
